1259126a183d780ad6656f37a79982b855b1f874
Krish4206 <mahajankrishna798@gmail.com>
2024-09-02 13:35:54 +0530

    Initial commi

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "./Components/HeroSection";
+import Navigation from "./Components/Navigation";
+import "./App.css"
+function App(){
+  return(
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  )
+};
+
+export default App;
---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,19 @@
+function Navigation(){
+    return(
+        <div className="container">
+        <nav>
+          <div className="logo">
+            <img src="/images/brand_logo.png" alt="logo"/>
+          </div>
+          <ul>
+            <li href="#">MENU</li>
+            <li href="#">LOCATION</li>
+            <li href="#">ABOUT</li>
+            <li href="#">PROJECT</li>
+          </ul>
+          <button>LOGIN</button>
+        </nav>
+      </div>
+    )
+};
+export default Navigation;
